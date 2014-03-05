d49709a58127b3537e587ab120d65a6c7bde09ac
neilrqm <neilrqm@gmail.com>
2014-03-04 20:25:59 -0800

    Added bare Atmel Studio project for guitar tuner code.

---
+++ src/guitar-tuner.c
@@ -0,0 +1,17 @@
+/*
+ * guitar_tuner.c
+ *
+ * Created: 04/03/2014 8:23:36 PM
+ *  Author: nrqm
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
